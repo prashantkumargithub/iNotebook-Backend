8361be15b425fba9d0625d702a1ddf2f21769358
prashantkumargithub <107783911+prashantkumargithub@users.noreply.github.com>
2024-06-15 13:19:53 +0530

    Updating

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ connectToMongo();
 const app = express();
 const port =  process.env.PORT || 5000;
 
-const Welcome = fs.readFileSync("welcome.html","utf-8");
+const Welcome = fs.readFileSync("./static/welcome.html","utf-8");
 
 app.use(cors())
 app.use(express.json())
@@ -27,5 +27,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`iNotebook backend server app listening on port ${port}`)
+  console.log(`iNotebook backend server app listening on http://localhost:${port}`)
 })
