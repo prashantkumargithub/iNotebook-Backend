59b6bea0fb1277eea8fe31ab7953a3226ba4909d
prashantkumargithub <107783911+prashantkumargithub@users.noreply.github.com>
2024-06-15 12:54:23 +0530

    Updating Welcome page

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 require('dotenv').config();
 const connectToMongo = require('./db');
 const express = require('express');
+const fs = require("fs");
 var cors = require('cors');
+
 
 connectToMongo();
 
 const app = express();
 const port =  process.env.PORT || 5000;
 
+const Welcome = fs.readFileSync("welcome.html","utf-8");
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Welcome');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(Welcome);
 });
 
 
